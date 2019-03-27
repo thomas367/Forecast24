e078d31d9f1b45d36e2581c9302da7833b65ec0c
thomas367 <thomas.367@hotmail.com>
2019-03-27 12:03:58 +0200

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <header-app/>
+    
+  </div>
+</template>
+
+<script>
+  import Header from './components/header.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      'header-app': Header
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/services/forecastData.js
@@ -0,0 +1,31 @@
+export default class ForecastData{
+	
+	constructor(){
+		this.baseUrl = 'http://api.openweathermap.org/data/2.5/'
+		this.api = 'weather'
+		this.apiKey = 'e5cdbd515249b28526ac579afe58f4fa'
+		this.query = 'q='
+		this.apiUrl = '${this.baseUrl}${this.api}?${this.query}&APPID=${this.apiKey}&units=metric';
+	}
+
+	rebuildApiUrl(){
+		this.apiUrl = '${this.baseUrl}${this.api}?${this.query}&APPID=${this.apiKey}&units=metric';
+	}
+
+	currentWeather(city){
+		this.query = 'q=' + city
+		this.api = 'weather'
+		this.rebuildApiUrl();
+
+		return this.apiUrl;
+	}
+
+	weekForecast(city){
+		this.query = 'q=' + city
+		this.api = 'forecast'
+		this.rebuildApiUrl();
+
+		return this.apiUrl;
+	}
+
+}
