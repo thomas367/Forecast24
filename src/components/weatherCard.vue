07ecd4da9475f95c2ed4ee2bff24e90b57989adb
thomas367 <thomas.367@hotmail.com>
2019-03-28 16:00:19 +0200

    Set up basic UI

--- src/components/weatherCard.vue
+++ src/components/weatherCard.vue
@@ -1,67 +1,62 @@
 <template>
 	<div class="weather-card">
-		<div class="box">
-			<div class="media">
-				<div class="media-left">
-					<slot name="icon"></slot>
-				</div>
-				<div class="media-content">
-					<div class="content">
-						<p>
-	                        <span class="title"><slot name="main"></slot></span>
-	                        <br>
-	                        <span><slot name="description"></slot></span>
-	                    </p>
-					</div>
-				    <!--	
-				    <h2 class="title" v-show="foreCast">
-	                    <span class="icon"><i class="fa fa-clock-o"></i></span>
-	                    <span>{{ foreCastTimeOfDay }}</span>
-	                </h2>  
-	                -->
-                	<div class="level">
-                        <div class="level-item">
-                        <!--    <p class="heading"><span v-show="!foreCast">Current</span> Temperature</p>  -->
-                            <p class="title"><slot name="curr_temp"></slot> °C</p>
-                        </div>
-                        <div class="level-item">
-                            <p class="heading">Min Temperature</p>
-                            <p class="title"><slot name="min_temp"></slot> °C</p>
-                        </div>
-                        <div class="level-item">
-                            <p class="heading">Max Temperature</p>
-                            <p class="title"><slot name="max_temp"></slot> °C</p>
-                        </div>
-                        <div class="level-item ">
-                            <p class="heading">Humidity</p>
-                            <p class="title"><slot name="humidity"></slot> %</p>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="level">
-                        <div class="level-item has-">
-                            <p class="heading">Current pressure</p>
-                            <p class="title"><slot name="pressure"></slot> hPa</p>
-                        </div>
-                        <div class="level-item">
-                            <p class="heading">Wind speed</p>
-                            <p class="title"><slot name="wind_speed"></slot> m/s</p>
-                        </div>
-                        <div class="level-item">
-                            <p class="heading">Wind direction</p>
-                            <p class="title"><slot name="wind_deg"></slot></p>
-                        </div>
-                    </div>
-				</div>
-			</div>
+		<div class="weatherIcon">
+			<slot name="icon"></slot>
+		</div>
+		<div class="sm-desc">
+			<p>
+	            <span class="title"><slot name="main"></slot></span>
+	            <br>
+	            <span><slot name="description"></slot></span>
+	        </p>
+				
+			<h2 class="title" v-show="foreCast">
+	        <!--    <span class="icon"> TODO: Add watch icon.</span>  -->
+	        <span>{{ foreCastTimeOfDay }}</span>
+	        </h2>    
+	            
+            <div class="row">
+                <div class="col-3 content">
+                    <p class="heading"><span v-show="!foreCast">Current</span>Temperature</p>
+                    <p class="value"><slot name="curr_temp"></slot> °C</p>
+                </div>
+                <div class="col-3 content">
+                    <p class="heading">Min Temperature</p>
+                    <p class="value"><slot name="min_temp"></slot> °C</p>
+                </div>
+                <div class="col-3 content">
+                    <p class="heading">Max Temperature</p>
+                    <p class="value"><slot name="max_temp"></slot> °C</p>
+                </div>
+                <div class="col-3 content">
+                    <p class="heading">Humidity</p>
+                    <p class="value"><slot name="humidity"></slot> %</p>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-4 content">
+                    <p class="heading">Current pressure</p>
+                    <p class="value"><slot name="pressure"></slot> hPa</p>
+                </div>
+                <div class="col-4 content">
+                    <p class="heading">Wind speed</p>
+                    <p class="value"><slot name="wind_speed"></slot> m/s</p>
+                </div>
+                <div class="col-4 content">
+                    <p class="heading">Wind direction</p>
+                    <p class="value"><slot name="wind_deg"></slot></p>
+                </div>
+            </div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import dateFormat from 'date-format'
 
 	export default{
-	/*	props: {
+		props: {
 			foreCast: {
 				type: Boolean,
 				default: false
@@ -71,13 +66,15 @@
 			}
 		},
 		computed: {
-			foreCastDate: function(){
+			/* Set date format */
+			foreCastDateTime: function(){
 				return dateFormat('dd/MM/yyyy', new Date(this.foreCastDate));
 			},
+			/* Set time format */
 			foreCastTimeOfDay: function(){
 				return dateFormat('hh:mm:ss', new Date(this.foreCastDate));
 			}
-		}  */
+		} 
 	}
 </script>
 
@@ -85,6 +82,10 @@
 	.weather-card{
 		margin-bottom: 10px;
 
+	}
+
+	.content {
+		text-align: center;
 	}
 
 </style>
